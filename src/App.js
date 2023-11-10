935e8d5fced7040839d7dccb51698e0c1684af64
Mingliang-Xu <oliver.xml@hotmail.com>
2023-11-10 14:03:55 -0800

    Unit Project Finished

--- src/App.js
+++ src/App.js
@@ -4,6 +4,27 @@ import "./App.css";
 import Square from "./Square";
 import CalculateWinner from "./CalculateWinner";
 
+// function CalculateWinner(arr) {
+//   let lines = [
+//     [0, 1, 2],
+//     [3, 4, 5],
+//     [6, 7, 8],
+//     [0, 3, 6],
+//     [1, 4, 7],
+//     [2, 5, 8],
+//     [0, 4, 8],
+//     [2, 4, 6],
+//   ];
+//   for (let i = 0; i < lines.length; i++) {
+//     console.log(lines[i]);
+//     const [a, b, c] = lines[i];
+//     if (arr[a] && arr[a] === arr[b] && arr[a] === arr[c]) {
+//       return `${arr[a]} won!`;
+//     }
+//   }
+//   return `Who will won?`;
+// }
+
 function App() {
   const [squares, setSquares] = useState(["", "", "", "", "", "", "", "", ""]);
   const [player, setPlayer] = useState(true);
@@ -14,7 +35,8 @@ function App() {
   };
   return (
     <div className="App">
-      <CalculateWinner squares={squares} />
+      {/* <span>{CalculateWinner(squares)}</span> */}
+      <CalculateWinner squares={squares}></CalculateWinner>
       <button onClick={handleClick}>Reset</button>
       <div className="container">
         {squares.map((value, index) => {
--- src/CalculateWinner.jsx
+++ src/CalculateWinner.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 const CalculateWinner = (props) => {
   const [result, setResult] = useState("Who will win?");
+
   let lines = [
     [0, 1, 2],
     [3, 4, 5],
@@ -13,23 +14,17 @@ const CalculateWinner = (props) => {
     [2, 4, 6],
   ];
   for (let i = 0; i < lines.length; i++) {
-    console.log(lines[i]);
     const [a, b, c] = lines[i];
     if (
       props.squares[a] &&
       props.squares[a] === props.squares[b] &&
-      props.squares[a] === props.squares[c]
+      props.squares[a] === props.squares[c] &&
+      result !== `${props.squares[a]} won !`
     ) {
-      console.log(
-        props.squares[a],
-        props.squares[b],
-        props.squares[c],
-        "--------"
-      );
-      setResult(`${props.squares[a]} won!`);
+      setResult(`${props.squares[a]} won !`);
+      break;
     }
   }
-  console.log("calculate winner");
   return <span>{result}</span>;
 };
 export default CalculateWinner;
